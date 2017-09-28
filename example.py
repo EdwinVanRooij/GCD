923b9a2484e7fbb7a2b64b7c49e7194c6c544fd3
Edwin <edwinvrooij@gmail.com>
2017-09-28 14:34:25 +0200

    got some results

--- example.py
+++ example.py
@@ -8,17 +8,17 @@ import glob
 #         "Couldn't put Humpty together again",
 #         ]
 
-# all_files = glob.glob('Gutenberg/Gutenberg Small/*.*')
-all_files = glob.glob('Gutenberg/TestDir/*.*')
+all_files = glob.glob('Gutenberg/Gutenberg Small/*.*')
+# all_files = glob.glob('Gutenberg/TestDir/*.*')
 
 
 def file_contents(file_name):
-    with open(file_name) as f:
+    with open(file_name, encoding="ISO-8859-1") as f:
         # f = open(file_name, encoding="ISO-8859-1")
         return f.read()
 
 
-datasource = dict((file_name, file_contents(file_name)) for file_name in all_files)
+data = dict((file_name, file_contents(file_name)) for file_name in all_files)
 
 
 def mapfn(k, v):
@@ -36,10 +36,40 @@ def reducefn(k, vs):
 s = mincemeat.Server()
 
 # The data source can be any dictionary-like object
-s.datasource = dict(datasource)
-print("Datasource:", dict(datasource))
+s.datasource = data
+# print("Datasource:", data)
 s.mapfn = mapfn
 s.reducefn = reducefn
 
 results = s.run_server(password="changeme")
 print(results)
+
+# # !/usr/bin/env python
+# import mincemeat
+#
+# data = ["Humpty Dumpty sat on a wall",
+#         "Humpty Dumpty had a great fall",
+#         "All the King's horses and all the King's men",
+#         "Couldn't put Humpty together again",
+#         ]
+#
+# def mapfn(k, v):
+#     for w in v.split():
+#         yield w, 1
+#
+# def reducefn(k, vs):
+#     result = 0
+#     for v in vs:
+#         result += v
+#     return result
+#
+# s = mincemeat.Server()
+#
+# # The data source can be any dictionary-like object
+# s.datasource = dict(enumerate(data))
+# print(s.datasource)
+# s.mapfn = mapfn
+# s.reducefn = reducefn
+#
+# results = s.run_server(password="changeme")
+# print(results)
